afd94a852106df67df40fb75a8203970e3d0f34f
amessoudi <amessoudi150@gmail.com>
2023-03-17 08:38:51 -0700

    finally betty passed test

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,23 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	
 	n = rand() - RAND_MAX / 2;
-	
 	if (n < 0)
-	       	printf("%d is negative\n", n);
-  	 
+		printf("%d is negative\n", n);
 	else if (n > 0)
 		printf("%d is positive\n", n);
-   	
-   	else
+	else
 		printf("%d is zero\n", n);
 	return (0);
 }
