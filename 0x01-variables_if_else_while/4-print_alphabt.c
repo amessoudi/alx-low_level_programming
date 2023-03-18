09e469ffbc842b54c2aa30f59a9a6ec6b55b8dc3
amessoudi <amessoudi150@gmail.com>
2023-03-18 02:48:19 -0700

    excluding e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z' && c !='q' && c!='e'; ++c)
+	for (c = 'a'; c <= 'z' && c != 'q' ||  c != 'e'; ++c)
 		putchar(c);
 	putchar('\n');
 	return (0);
