8bb29888da70353b5e259f4284e32ac2b13f2cd9
amessoudi <amessoudi150@gmail.com>
2023-03-17 17:52:44 -0700

    Alphabet putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This code prints the alphabet */
+/* The alphabet trick*/
+int main(void) /* Is this how you  describe it betty+? */
+{
+	char c;
+
+	for (c = 'a', c <= 'z', ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
