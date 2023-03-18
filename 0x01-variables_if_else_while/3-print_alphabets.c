6324b63f3776ca94bcda27910b740cbc3385cbed
amessoudi <amessoudi150@gmail.com>
2023-03-18 01:42:38 -0700

    added uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - print alphabet
+  * Prints from a to z then from A to Z
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		putchar(c);
+	for (C = 'A'; C <= 'Z', ++C)
+	putchar('\n');
+	return (0);
+}
