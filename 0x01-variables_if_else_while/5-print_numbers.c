0489cc5dc3fdd8f26bd699a837d3cb2da35a3b72
amessoudi <amessoudi150@gmail.com>
2023-03-18 02:50:49 -0700

    new comit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,9 +9,12 @@ int main(void)
 
 {
 	char c;
+	char C;
 
-	for (c = 'a';(c <= 'z' && c != 'q') ||  (c != 'e' c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; ++c)
 		putchar(c);
+	for (C = 'A'; C <= 'Z'; ++C)
+		putchar(C);
 	putchar('\n');
 	return (0);
 }
