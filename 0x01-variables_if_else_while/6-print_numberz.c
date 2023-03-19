b0045880ceae9edbb24c9230876c9f0ba2bb6ff2
amessoudi <amessoudi150@gmail.com>
2023-03-19 15:31:37 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print numberz
+  *
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	int n;
+
+	for (n = 0; n < 10; ++n)
+		putchar("%d \n", n);
+	putchar('\n');
+	return (0);
+}
