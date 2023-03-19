9fe3ffef96927358b0521294d1e8d9684a6d01c7
amessoudi <amessoudi150@gmail.com>
2023-03-19 15:45:43 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print numberz
+  *
+  *
+  * Return: Always 0
+  */
+int main(void)
+
+{
+	int n;
+
+	for (n = 122; n > 97; --n)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
