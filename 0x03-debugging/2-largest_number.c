b7ad9bbd80b2107084e8fd7e12256dadacc90722
amessoudi <amessoudi150@gmail.com>
2023-03-24 08:13:21 -0700

    new commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,9 +2,11 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
+ *
  * @a: first integer
  * @b: second integer
  * @c: third integer
+ *
  * Return: largest number
  */
 
@@ -27,4 +29,3 @@ int largest_number(int a, int b, int c)
 
 	return (largest);
 }
-
