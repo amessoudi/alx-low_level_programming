9abdc019bf74fcff25852b1271ffac2a3f0841f7
Ahmed <amessoudi150@gmail.com>
2023-09-23 15:07:15 -0700

    printing alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints whole alphabet from a to z lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	n = 97;
+	while (n <123)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
