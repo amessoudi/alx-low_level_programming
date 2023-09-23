f869212c3b51642bebc4509ae1278391ff0edb49
Ahmed <amessoudi150@gmail.com>
2023-09-23 13:04:12 -0700

    my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *This fuction prints an exact string
+ *
+ *The return is Zero
+ */
+
+int main() {
+	puts("Programming is like building a multilingual puzzle");
+	return = 0;
+}
