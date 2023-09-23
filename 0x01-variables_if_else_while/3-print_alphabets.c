4bfd7c1d7a30b99313bb76104de6c54d8eb352cf
Ahmed <amessoudi150@gmail.com>
2023-09-23 15:19:15 -0700

    I'm still  using while instead of for

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints whole alphabet from a to z lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	
+	n = 65;
+
+	while (n < 91)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
