439e735b5b460768be6c1482448417ffae1aab02
Ahmed <amessoudi150@gmail.com>
2023-09-23 14:58:09 -0700

    fixed output 0 instead of zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,18 +16,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	l = n % 10;
-	printf("Last digit of %d is %d ", n,l);
+	printf("Last digit of %d is %d ", n, l);
 	if (n > 5)
 	{
 		printf("and is greater than 5\n");
 	}
 	else if (n == 0)
 	{
-		printf("and is zero\n");
+		printf("and is 0\n");
 	}
 	else if (n < 6)
 	{
-		printf("and is less than 6 and not zero\n");
+		printf("and is less than 6 and not 0\n");
 	}
 	return (0);
 }
