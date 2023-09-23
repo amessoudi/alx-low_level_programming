a0390ce41910c3f6ed529cb436e35a4bb5020a5f
Ahmed <amessoudi150@gmail.com>
2023-09-23 13:28:19 -0700

    Now we're using printf

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Description: the programs prints a string using printf
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
