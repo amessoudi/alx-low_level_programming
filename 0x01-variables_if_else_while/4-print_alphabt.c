234b38884220aa838abb690181ccf2522268ddf0
Ahmed <amessoudi150@gmail.com>
2023-09-24 05:43:12 -0700

    fixed trailing space

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	char n;
 
-	for ( n = 'a'; n <= 'z'; n++)
+	for (n = 'a'; n <= 'z'; n++)
 	{	
-	       	if ( n != 'e' && n != 'q')
+		if (n != 'e' && n != 'q')
 			putchar(n);
 	}
 	putchar('\n');
