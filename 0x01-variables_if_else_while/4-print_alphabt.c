78693c1522a745aa6d2b59ef032073fe9e1f8729
Ahmed <amessoudi150@gmail.com>
2023-09-24 05:21:02 -0700

    alphabet without QE

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description: prints lowercase alphabet escept "e" and "q"
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for ( n = 'a'; n <= 'z'; n++)
+	{	
+	       	if ( n != 'e' & n != 'q')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
