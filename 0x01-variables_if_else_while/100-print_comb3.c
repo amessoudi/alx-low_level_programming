cc7a239745b066e1248702abd673c2259756a7d0
Ahmed <amessoudi150@gmail.com>
2023-09-24 09:30:02 -0700

    printing combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all combination of single digit numbers
+ * Return:  Always 0 (succss)
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 48;
+	n = 49;
+
+	for (; n <= 57; n++)
+	{
+		for (; (m <= 57) && (m != n); n++)
+		{
+			putchar(n);
+			putchar(m);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
