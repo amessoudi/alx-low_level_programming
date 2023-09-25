5acfb7edc30aec7c096be86e1b20a3271943357f
amessoudi <amessoudi150@gmail.com>
2023-09-25 20:49:31 +0100

    using different method

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,17 +11,19 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int j;
+	int i, j;
+	i = 0;
+	j = 0;
 
-	for (i = 0; i < 8; i++)
+	while (i < 64)
 	{
-		for (j = 0; j < 8; j++)
+		if (i % 8 == 0 && i != 0)
 		{
-			_putchar(a[i][j]);
-			if (j != 7)
-				_putchar(' ');
+			j = i;
+			putchar('\n');
 		}
-		_putchar('\n');
+		_putchar(a[i / 8][ i - j]);
+		i++;
 	}
+	putchar('\n');
 }
