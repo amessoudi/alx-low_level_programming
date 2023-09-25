1069387c8fa31bb6c020e037c01c10bf7060e0e3
Ahmed <amessoudi150@gmail.com>
2023-09-25 07:21:23 -0700

    fixed for betty and code

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * memeset - entry point
+ *_memset - entry point
+ *@s: the starting point
+ *@b: the constant byte
+ *@n: the number of boxes affected in memory
  *
- *Description: definition of memeset function
- *Return : Always S (success)
+ *Description: definition of memeset functio
+ *
+ *Return: Always S (success)
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
