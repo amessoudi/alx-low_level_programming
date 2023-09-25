36524cf2ad19d597a42f80b47c11ea5cea2302a8
amessoudi <amessoudi150@gmail.com>
2023-09-25 15:01:23 +0100

    First task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * memeset - entry point
+ *
+ *Description: definition of memeset function
+ *Return : Always S (success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
