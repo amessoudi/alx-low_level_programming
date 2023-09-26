54877de80c7c7cad9524cb643f3a949aa69c7e96
amessoudi <amessoudi150@gmail.com>
2023-09-27 00:38:37 +0100

    times tables..

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - Prints natural numbers from n to 98, followed by a new line.
  * @n: The starting number.
  */
 void print_to_98(int n)
