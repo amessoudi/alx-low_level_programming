dc51b44667f7e09a1f02dcd7d47e69df744e2bdb
amessoudi <amessoudi150@gmail.com>
2023-09-26 23:30:26 +0100

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet lowercase
+*/
+
+void print_alphabet(void)
+{
+    char i,
+
+    for (i = 'a'; i <= 'z'; i++)
+        _putchar(c);
+    _putchar('\n')
+}
