86fd01f8cb106f35415ca20e726c85cf98a24169
amessoudi <amessoudi150@gmail.com>
2023-09-27 17:54:31 +0100

    printing diagonally

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,12 @@
 
 void print_diagonal(int n)
 {
+    int i, j;
 	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-
-	int i, j;
-
 	for (i = 0; i < n; i++)
 	{
 		/* Print spaces for indentation */
