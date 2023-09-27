795f812db33c0312e05ef750afe48ca667fa5cdf
amessoudi <amessoudi150@gmail.com>
2023-09-27 17:51:21 +0100

    diagonaly printing n

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* print_diagonal - Prints diagonaly.
+*@n: the numbers of rows
+*
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		/* Print spaces for indentation */
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+}
