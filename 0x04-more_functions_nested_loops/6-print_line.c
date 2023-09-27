af060a6d4e556b33f5ba4b73c37eb5f11387dc6c
amessoudi <amessoudi150@gmail.com>
2023-09-27 17:59:32 +0100

    printing a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_line - a line
+*@n: the numbers of rows
+*
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
