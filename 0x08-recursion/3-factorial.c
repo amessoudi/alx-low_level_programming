ed00b20e0560ca8f09fbc6939537fb4574ce4266
amessoudi <amessoudi150@gmail.com>
2023-09-29 00:51:45 +0100

    I think I understand what reccursion is...

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*factorial - main entry
+*@n: the number to be factorialized
+*Return: factorial of n
+*/
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		n = (n * factorial(n - 1));
+		return (n);
+	}
+	else
+	{
+		return (1);
+	}
+}
