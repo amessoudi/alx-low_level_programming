742cd23e8fd6bcfbf8b0076623da17c2149d9237
amessoudi <amessoudi150@gmail.com>
2023-09-29 00:08:31 +0100

    print reverse fnct

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_print_rev_recursion - main entry
+*@s: the string to be printed
+*/
+
+void _print_rev_recursion(char *s);
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(++s);
+	putchar(*s);
+}
