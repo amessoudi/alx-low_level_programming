cef22f98852a1b098757b2dda58d5481c15649d1
amessoudi <amessoudi150@gmail.com>
2023-10-09 20:19:45 +0100

    mul.c fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,76 +1,134 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_digit - checks if a string is a digit
- * @s: string to check
- * Return: 0 if false, 1 if true
- */
-int is_digit(char *s)
-{
-	int result = 1;
+ * _memset - fills memory with a constant byte
+ *
+ * @s: input pointer that represents memory block
+ *     to fill
+ * @b: characters to fill/set
+ * @n: number of bytes to be filled
+ *
+ * Return: pointer to the filled memory area
+*/
 
-	while (*s)
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		if (*s < '0' || *s > '9')
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - function that allocates memory
+ *           for an array using memset
+ *
+ * @nmemb: size of array
+ * @size: size of each element
+ *
+ * Return: pointer to new allocated memory
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
+}
+
+
+/**
+ * multiply - initialize array with 0 byte
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: nothing
+*/
+
+void multiply(char *s1, char *s2)
+{
+	int i, l1, l2, total_l, f_digit, s_digit, res = 0, tmp;
+	char *ptr;
+	void *temp;
+
+	l1 = _length(s1);
+	l2 = _length(s2);
+	tmp = l2;
+	total_l = l1 + l2;
+	ptr = _calloc(sizeof(int), total_l);
+
+	/* store our pointer address to free later */
+	temp = ptr;
+
+	for (l1--; l1 >= 0; l1--)
+	{
+		f_digit = s1[l1] - '0';
+		res = 0;
+		l2 = tmp;
+		for (l2--; l2 >= 0; l2--)
 		{
-			result = 0;
-			break;
+			s_digit = s2[l2] - '0';
+			res += ptr[l2 + l1 + 1] + (f_digit * s_digit);
+			ptr[l1 + l2 + 1] = res % 10;
+			res /= 10;
 		}
-		s++;
+		if (res)
+			ptr[l1 + l2 + 1] = res % 10;
 	}
-	return (result);
-}
 
-/**
- * print_string - prints a string using _putchar
- * @str: string to print
- */
-void print_string(char *str)
-{
-	while (*str)
+	while (*ptr == 0)
 	{
-		_putchar(*str);
-		str++;
+		ptr++;
+		total_l--;
 	}
+
+	for (i = 0; i < total_l; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+	free(temp);
 }
 
-/**
- * mul - multiplies two numbers
- * @num1: first number
- * @num2: second number
- * Return: result
- */
-char *mul(char *num1, char *num2)
-{
-	return (NULL);
-}
 
 /**
- * main - multiplies two positive numbers and prints the result.
- * @argc: argument count
- * @argv: argument vectors
- * Return: 0 on success, 98 on error
- */
+ * main - Entry point
+ *
+ * Description: a program that multiplies
+ *            two positive numbers
+ *
+ * @argc: number of arguments
+ * @argv: arguments array
+ *
+ * Return: 0 on success 98 on faliure
+*/
+
 int main(int argc, char *argv[])
 {
-	char *result;
+	char *n1 = argv[1];
+	char *n2 = argv[2];
 
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	if (argc != 3 || check_number(n1) || check_number(n2))
+		error_exit();
+
+	if (*n1 == '0' || *n2 == '0')
 	{
-		print_string("Error\n");
-		exit(98);
+		_putchar('0');
+		_putchar('\n');
 	}
-
-	result = mul(argv[1], argv[2]);
-	if (result == NULL)
-	{
-		print_string("Error\n");
-		exit(98);
-	}
-
-	print_string(result);
-	_putchar('\n');
-	free(result);
+	else
+		multiply(n1, n2);
 	return (0);
 }
