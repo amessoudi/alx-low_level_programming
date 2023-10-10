4cf8397bfd2b387dae287eea35822f8a61f40731
amessoudi <amessoudi150@gmail.com>
2023-10-10 18:52:10 +0100

    dog-t is defined

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,13 @@
  *
  * Description: Data structure to store information about a dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} Dog;
-
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
