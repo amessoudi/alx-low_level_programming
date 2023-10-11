1b9961f710435dd1ea9d23012398f74bd717ee8f
amessoudi <amessoudi150@gmail.com>
2023-10-11 20:28:08 +0100

    My program is working..(I hope)

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function for the calculator program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, error code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL || (argv[2][1] != '\0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", operation(a, b));
+	return (0);
+}
