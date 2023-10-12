5d29bef80ac814f2af494889bf114765dc444127
amessoudi <amessoudi150@gmail.com>
2023-10-12 20:50:26 +0100

    Let's try again ! (update)

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,7 +8,6 @@ void _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int _putchar(char c);
 void print_int(int n);
 void print_str(char *s);
 
