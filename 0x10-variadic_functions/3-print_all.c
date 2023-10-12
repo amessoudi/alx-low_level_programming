6302256ff5265ef3690a8192ef89503c9d65b4c8
amessoudi <amessoudi150@gmail.com>
2023-10-12 21:02:34 +0100

    used printf

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stddef.h>
 #include <stdio.h>
 
 /**
  * print_all - prints anything.
  * @format: A list of types of arguments passed to the function.
- *
+ * 
  * Return: void.
  */
 void print_all(const char * const format, ...)
@@ -22,60 +21,28 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 		case 'c':
-			putchar(va_arg(args, int));
+			printf("%c", va_arg(args, int));
 			break;
 		case 'i':
-			print_int(va_arg(args, int));
+			printf("%d", va_arg(args, int));
 			break;
 		case 'f':
+			printf("%f", va_arg(args, double));
 			break;
 		case 's':
 			str = va_arg(args, char*);
 			if (!str)
 				str = "(nil)";
-			print_str(str);
+			printf("%s", str);
 			break;
 		}
 
 		if ((format[i] == 'c' || format[i] == 'i' ||
 			format[i] == 'f' || format[i] == 's') && format[i + 1])
-		{
-			putchar(',');
-			putchar(' ');
-		}
-
+			printf(", ");
+		
 		i++;
 	}
-	putchar('\n');
+	printf("\n");
 	va_end(args);
-}
-
-/**
- * print_int - prints an integer.
- * @n: The integer.
- *
- * Return: void.
- */
-void print_int(int n)
-{
-	if (n < 0)
-	{
-		putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_int(n / 10);
-	putchar(n % 10 + '0');
-}
-
-/**
- * print_str - prints a string.
- * @s: The string.
- *
- * Return: void.
- */
-void print_str(char *s)
-{
-	while (*s)
-		putchar(*s++);
 }
