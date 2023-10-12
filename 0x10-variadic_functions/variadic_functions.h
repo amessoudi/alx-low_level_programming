bad8df3e1834eaaff0c2161b7181d1dfdce9b224
amessoudi <amessoudi150@gmail.com>
2023-10-12 19:35:08 +0100

    included lib

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,9 @@
 #ifndef VARDIADIC_FUNCTION_H
 #define VARDIADIC_FUNCTION_H
 
+#include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
-void putchar_print(char *str);
+void _putchar(char *str);
 
 #endif VARDIADIC_FUNCTION_H /* VARDIADIC_FUNCTION_H */
