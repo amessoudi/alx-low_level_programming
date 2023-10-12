2560c3012b02a44d522291828ff4de73cf43c406
amessoudi <amessoudi150@gmail.com>
2023-10-12 20:48:16 +0100

    One print to rule them all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,80 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+
+/**
+ * print_all - prints anything.
+ * @format: A list of types of arguments passed to the function.
+ *
+ * Return: void.
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			_putchar(va_arg(args, int));
+			break;
+		case 'i':
+			print_int(va_arg(args, int));
+			break;
+		case 'f':
+			break;
+		case 's':
+			str = va_arg(args, char*);
+			if (!str)
+				str = "(nil)";
+			print_str(str);
+			break;
+		}
+
+		if ((format[i] == 'c' || format[i] == 'i' ||
+			format[i] == 'f' || format[i] == 's') && format[i + 1])
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+		i++;
+	}
+	_putchar('\n');
+	va_end(args);
+}
+
+/**
+ * print_int - prints an integer.
+ * @n: The integer.
+ *
+ * Return: void.
+ */
+void print_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+		print_int(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+/**
+ * print_str - prints a string.
+ * @s: The string.
+ *
+ * Return: void.
+ */
+void print_str(char *s)
+{
+	while (*s)
+		_putchar(*s++);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,5 +7,9 @@ int sum_them_all(const unsigned int n, ...);
 void _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
+void print_int(int n);
+void print_str(char *s);
 
 #endif
