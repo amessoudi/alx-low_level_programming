f20811e7d0d3c18a0858df0a31928ea2f39a2980
amessoudi <amessoudi150@gmail.com>
2023-10-18 16:14:18 +0100

    fixed main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+int calculate_day_of_year(int month, int day, int is_leap);
+void print_remaining_days(int month, int day, int year);
+
 
 #endif /* MAIN_H */
