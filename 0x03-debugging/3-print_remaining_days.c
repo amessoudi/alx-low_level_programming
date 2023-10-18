ddce4d2849e22b31aa0149c473e1270595cb3a6b
amessoudi <amessoudi150@gmail.com>
2023-10-18 16:19:33 +0100

    If only augusts weren't jealous of Julius !

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,63 +2,23 @@
 #include "main.h"
 
 /**
- * calculate_day_of_year - Calculate the nth day of the year.
- * @month: month of the year.
- * @day: day of the month.
- * @is_leap: whether the year is a leap year.
- * Return: day of the year.
- */
-int calculate_day_of_year(int month, int day, int is_leap)
-{
-	int total_days = day;
-
-	if (is_leap && month > 2)
-	{
-		total_days++;
-	}
-
-	if (month > 1)
-		total_days += 31;
-	if (month > 2)
-		total_days += is_leap ? 29 : 28;
-	if (month > 3)
-		total_days += 31;
-	if (month > 4)
-		total_days += 30;
-	if (month > 5)
-		total_days += 31;
-	if (month > 6)
-		total_days += 30;
-	if (month > 7)
-		total_days += 31;
-	if (month > 8)
-		total_days += 31;
-	if (month > 9)
-		total_days += 30;
-	if (month > 10)
-		total_days += 31;
-	if (month > 11)
-		total_days += 30;
-
-	return (total_days);
-}
-
-/**
  * print_remaining_days - Print the remaining days of the year.
  * @month: month of the year.
- * @day: day of the month.
+ * @day: day of the year.
  * @year: the current year.
  * Return: void.
  */
 void print_remaining_days(int month, int day, int year)
 {
 	int is_leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-	int total_days = calculate_day_of_year(month, day, is_leap);
+
+	if (is_leap && month > 2)
+		day++;
 
 	if (is_leap)
 	{
-		printf("Day of the year: %d\n", total_days);
-		printf("Remaining days: %d\n", 366 - total_days);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
@@ -68,8 +28,8 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
-			printf("Day of the year: %d\n", total_days);
-			printf("Remaining days: %d\n", 365 - total_days);
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
 }
