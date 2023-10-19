54ca82eea1ec6c8d9c5ff7c66d05162785662305
amessoudi <amessoudi150@gmail.com>
2023-10-19 18:35:59 +0100

    fixed lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+typedef struct list_s
+{
+	char *str;
+	struct list_s *next;
+} list_t;
+
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void print_list(const list_t *head);
