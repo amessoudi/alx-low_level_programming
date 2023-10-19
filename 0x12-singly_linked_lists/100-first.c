b3fbe8dffe9fa25da5748a9928e119d3ebdc4d27
amessoudi <amessoudi150@gmail.com>
2023-10-19 18:28:00 +0100

    Rabbit wins thanks to attribute constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - Prints a message before the main function is executed.
+ */
+void __attribute__((constructor)) print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
