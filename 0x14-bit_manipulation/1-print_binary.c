9647fd6bb860fe02f64d484a510c996db5fcca29
amessoudi <amessoudi150@gmail.com>
2023-10-27 02:17:13 +0100

    fixing..(ed?)

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,8 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to be printed in binary
- *
- * Return: void
  */
 void print_binary(unsigned long int n)
 {
