d1f6d5ac541b941916e6332b9230c1bbb1f21e06
amessoudi <amessoudi150@gmail.com>
2023-10-30 16:15:18 +0100

    copying can be daunting

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,18 @@
 #define MAIN_H
 
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_file(int fd, char *filename);
+void copy_content(int fd_from, int fd_to, char *file_from, char *file_to);
+
 int _putchar(char c);
 
 #endif /* MAIN_H */
