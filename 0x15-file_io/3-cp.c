1a65ec674bc1fb0698595a5bb85dd6192e351edc
amessoudi <amessoudi150@gmail.com>
2023-10-31 14:36:05 +0100

    updated and fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,65 +1,32 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#define BUF_SIZE 1024
-
-void close_fd(int fd, const char *filename);
-int open_file(char *file, int flags, mode_t mode);
-void copy_content(int fd_from, int fd_to, char *file_from, char *file_to);
+#include "main.h"
 
 /**
  * close_fd - Closes a file descriptor and checks for errors
  * @fd: File descriptor to close
  * @filename: Name of the file associated with the file descriptor
- *
- * Description: If closing the file descriptor fails, the function
- *              prints an error message to stderr and exits with code 100
  */
 void close_fd(int fd, const char *filename)
 {
 	if (close(fd) < 0)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't close fd %d for file %s\n", fd, filename);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d for file %s\n",
+				fd, filename);
 		exit(100);
 	}
 }
 
 /**
- * open_file - Opens a file and checks for errors
- * @file: Name of the file to open
- * @flags: Flags for opening the file
- * @mode: Mode for file creation
- * Return: File descriptor on success, exits on failure
- */
-int open_file(char *file, int flags, mode_t mode)
-{
-	int fd = open(file, flags, mode);
-
-	if (fd < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't %s %s\n",
-			(flags & O_RDONLY) ? "read from file" : "write to", file);
-		exit((flags & O_RDONLY) ? 98 : 99);
-	}
-	return (fd);
-}
-
-/**
  * copy_content - Copies content from one file to another
- * @fd_from: File descriptor of source file
- * @fd_to: File descriptor of destination file
- * @file_from: Name of source file
- * @file_to: Name of destination file
+ * @fd_from: File descriptor to copy from
+ * @fd_to: File descriptor to copy to
+ * @file_from: Name of the source file
+ * @file_to: Name of the destination file
  */
-void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
+void copy_content(int fd_from, int fd_to, const char *file_from,
+				  const char *file_to)
 {
-	ssize_t bytes_read, bytes_written;
 	char buffer[BUF_SIZE];
+	ssize_t bytes_read, bytes_written;
 
 	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
@@ -72,6 +39,7 @@ void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
 			exit(99);
 		}
 	}
+
 	if (bytes_read < 0)
 	{
 		close_fd(fd_from, file_from);
@@ -82,9 +50,10 @@ void copy_content(int fd_from, int fd_to, char *file_from, char *file_to)
 }
 
 /**
- * main - Copies the content of a file to another file
+ * main - Entry point, copies the content of a file to another file
  * @argc: Argument count
  * @argv: Argument vector
+ *
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
@@ -97,8 +66,21 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open_file(argv[1], O_RDONLY, 0);
-	fd_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to < 0)
+	{
+		close_fd(fd_from, argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
 	copy_content(fd_from, fd_to, argv[1], argv[2]);
 
 	close_fd(fd_from, argv[1]);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+#define BUF_SIZE 1024
 
+void close_fd(int fd, const char *filename);
+void copy_content(int fd_from, int fd_to, const char *file_from,
+				  const char *file_to);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 void close_fd(int fd, const char *filename);
