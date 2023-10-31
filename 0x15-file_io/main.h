a8bdae090c9b00727611dd278b25a645a665d213
amessoudi <amessoudi150@gmail.com>
2023-10-31 16:40:45 +0100

    fixed betty

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,14 +18,6 @@ void copy_content(int fd_from, int fd_to, const char *file_from,
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 void close_fd(int fd, const char *filename);
-int display_elf_header(const char *filename);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char e_class);
-void print_data(unsigned char e_data);
-void print_version(unsigned char e_version);
-void print_osabi(unsigned char e_osabi);
-void print_abiversion(unsigned char e_abiversion);
-void print_type(uint16_t e_type);
-void print_entry(uint64_t e_entry);
+
 
 #endif /* MAIN_H */
