cddefaefab9c09470f8e4489ced2a580f0bedf27
amessoudi <amessoudi150@gmail.com>
2023-10-31 14:48:28 +0100

    Elf still on the shelf !

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -29,8 +29,27 @@ int read_elf_header(int fd, Elf64_Ehdr *header)
  */
 void print_elf_header(Elf64_Ehdr *header)
 {
-	/* Implement the function to print the ELF header information */
-	/* Use printf to format and display the required information */
+	int i;
+
+	printf("Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header->e_ident[i]);
+	printf("\n");
+
+	printf("Class:                             ELF%d\n",
+		   header->e_ident[EI_CLASS] == ELFCLASS64 ? 64 : 32);
+	printf("Data:                              2's complement, ");
+	printf("%s\n",
+		   header->e_ident[EI_DATA] == ELFDATA2LSB ? "little endian" : "big endian");
+	printf("Version:                           %d (current)\n",
+		   header->e_version);
+	printf("OS/ABI:                            UNIX - System V\n");
+	printf("ABI Version:                       %d\n",
+		   header->e_ident[EI_ABIVERSION]);
+	printf("Type:                              %d\n",
+		   header->e_type);
+	printf("Entry point address:               0x%lx\n",
+		   (unsigned long)header->e_entry);
 }
 
 /**
