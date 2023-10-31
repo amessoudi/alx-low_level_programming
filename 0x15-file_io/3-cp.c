384a4d35225e39ce345c6cc3bd912f5494db2a56
amessoudi <amessoudi150@gmail.com>
2023-10-31 14:10:17 +0100

    Fixed for betty !

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,10 +17,10 @@
  */
 void close_fd(int fd, const char *filename)
 {
-    if (close(fd) < 0) {
-        dprintf(STDERR_FILENO, "Error: Can't close fd %d for file %s\n", fd, filename);
-        exit(100);
-    }
+	if (close(fd) < 0) {
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d for file %s\n", fd, filename);
+		exit(100);
+	}
 }
 
 /**
@@ -32,53 +32,53 @@ void close_fd(int fd, const char *filename)
  */
 int main(int argc, char *argv[])
 {
-    int fd_from, fd_to;
-    char buffer[BUF_SIZE];
-    ssize_t bytes_read, bytes_written;
+	int fd_from, fd_to;
+	char buffer[BUF_SIZE];
+	ssize_t bytes_read, bytes_written;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from < 0)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fd_to < 0)
-    {
-        close_fd(fd_from, argv[1]);
-        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-        exit(99);
-    }
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to < 0)
+	{
+		close_fd(fd_from, argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 
-    while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
-    {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written != bytes_read)
-        {
-            close_fd(fd_from, argv[1]);
-            close_fd(fd_to, argv[2]);
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    }
+	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+		{
+			close_fd(fd_from, argv[1]);
+			close_fd(fd_to, argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 
-    if (bytes_read < 0)
-    {
-        close_fd(fd_from, argv[1]);
-        close_fd(fd_to, argv[2]);
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
-    }
+	if (bytes_read < 0)
+	{
+		close_fd(fd_from, argv[1]);
+		close_fd(fd_to, argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-    close_fd(fd_from, argv[1]);
-    close_fd(fd_to, argv[2]);
+	close_fd(fd_from, argv[1]);
+	close_fd(fd_to, argv[2]);
 
-    return 0;
+	return 0;
 }
