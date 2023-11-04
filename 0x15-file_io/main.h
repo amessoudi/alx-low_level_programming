03492e20003cbc2eb1a3e70131bb1ac054e11170
amessoudi <amessoudi150@gmail.com>
2023-11-04 02:11:57 +0100

    fixed main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,7 @@
 #define BUF_SIZE 1024
 
 void close_fd(int fd, const char *filename);
+int create_file(const char *filename, char *text_content);
 void copy_content(int fd_from, int fd_to, const char *file_from,
 				  const char *file_to);
 ssize_t read_textfile(const char *filename, size_t letters);
